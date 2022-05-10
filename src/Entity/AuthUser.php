bdaaea1ed9bbb602d8450a590ee2238994cd339f
L337 <loneos1337@gmail.com>
2022-05-10 18:20:41 +0200

    added registration

--- src/Entity/AuthUser.php
+++ src/Entity/AuthUser.php
@@ -4,12 +4,14 @@ namespace App\Entity;
 
 use App\Repository\AuthUserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=AuthUserRepository::class)
  */
+#[UniqueEntity(fields: ['login'], message: 'There is already an account with this login')]
 class AuthUser implements UserInterface, PasswordAuthenticatedUserInterface
 {
     /**
@@ -18,45 +20,41 @@ class AuthUser implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Column(type="integer")
      */
     private $id;
-
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      */
     private $login;
-
     /**
      * @ORM\Column(type="json")
      */
     private $roles = [];
-
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
      */
     private $password;
-
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $name;
-
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
     public function getId(): ?int
     {
         return $this->id;
     }
-
     public function getLogin(): ?string
     {
         return $this->login;
     }
-
     public function setLogin(string $login): self
     {
         $this->login = $login;
 
         return $this;
     }
-
     /**
      * A visual identifier that represents this user.
      *
@@ -66,7 +64,6 @@ class AuthUser implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return (string) $this->login;
     }
-
     /**
      * @see UserInterface
      */
@@ -78,14 +75,12 @@ class AuthUser implements UserInterface, PasswordAuthenticatedUserInterface
 
         return array_unique($roles);
     }
-
     public function setRoles(array $roles): self
     {
         $this->roles = $roles;
 
         return $this;
     }
-
     /**
      * @see PasswordAuthenticatedUserInterface
      */
@@ -93,14 +88,12 @@ class AuthUser implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return $this->password;
     }
-
     public function setPassword(string $password): self
     {
         $this->password = $password;
 
         return $this;
     }
-
     /**
      * @see UserInterface
      */
@@ -109,15 +102,23 @@ class AuthUser implements UserInterface, PasswordAuthenticatedUserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
-
     public function getName(): ?string
     {
         return $this->name;
     }
-
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
 
         return $this;
     }
