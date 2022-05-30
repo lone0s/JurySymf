9a859d675f1fa21296cfed749adb9dd64f5c3383
L337 <loneos1337@gmail.com>
2022-05-30 18:00:00 +0200

    Added general management links navbar view

--- src/Controller/UniversiteController.php
+++ src/Controller/UniversiteController.php
@@ -7,7 +7,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-#[Route('/universite')]
+#[Route('/universite', name: 'universite')]
 class UniversiteController extends AbstractController
 {
     #[Route('', name: '_list')]
