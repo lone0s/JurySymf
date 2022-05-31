1f83434cb13cc12cecffc75e5a124f5d79fad1db
L337 <loneos1337@gmail.com>
2022-05-31 11:06:42 +0200

    Corrected controller redirection conflict

--- src/Controller/FrontEndController.php
+++ src/Controller/FrontEndController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('', name : 'redirect')]
 class FrontEndController extends AbstractController
 {
-    #[Route('/notes',name : '_to_notes_view')]
+    #[Route('/notes',name : '_to_all_grades')]
     public function redirectToGrades() : Response {
         return $this -> render('site/links/notes/grades_links.html.twig');
     }
@@ -23,7 +23,7 @@ class FrontEndController extends AbstractController
         return  $this -> render('site/links/universite/main_univ_links.html.twig');
     }
 
-    #[Route('/notes/{studentId}', '_to_student_grades')]
+    #[Route('/etudiants/notes/{studentId}', '_to_student_grades')]
     public function redirectToStudentGrades(int $studentId) : Response {
         $args = ['id' => $studentId];
         return $this -> render('site/links/notes/student_grades_links.html.twig', $args);
